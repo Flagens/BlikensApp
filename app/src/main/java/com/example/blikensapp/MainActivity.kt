ce1b703f429f960f77ff0c1cd731e9fc134354d5
michal <michalwitkowski52@gmail.com>
2023-01-12 22:36:38 +0100

    Added Display of the blikCode and in jsonbody.put there is creationDate and expirationDate

--- app/src/main/java/com/example/blikensapp/MainActivity.kt
+++ app/src/main/java/com/example/blikensapp/MainActivity.kt
@@ -1,27 +1,31 @@
 package com.example.blikensapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.blikensapp.databinding.ActivityMainBinding
 import org.json.JSONObject
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.HashMap
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding:ActivityMainBinding
+    val accessToken = "r392jnfv2-almf92nww1-ld:fwfa"
+
+    val url = "http://192.168.1.45:8080/api/blik/"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         binding =ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val accessToken = "r392jnfv2-almf92nww1-ld:fwfa"
-        //val url = "http://localhost:8080/api/blik/"
-        val url = "http://192.168.1.45:8080/api/blik/"
-        binding.button.setOnClickListener {
 
+        binding.button.setOnClickListener {
             getDataFromApi(url,accessToken)
 
         }
@@ -31,8 +35,8 @@ class MainActivity : AppCompatActivity() {
         val queue = Volley.newRequestQueue(this)
         val jsonBody = JSONObject()
         jsonBody.put("accountId","2")
-        jsonBody.put("creationDate","2022-12-07 23:00:00")
-        jsonBody.put("expirationDate","2022-12-07 23:11:11")
+        jsonBody.put("creationDate",getDate(0))
+        jsonBody.put("expirationDate",getDate(2))
 
         val requestBody = jsonBody.toString()
 
@@ -40,6 +44,7 @@ class MainActivity : AppCompatActivity() {
             Response.Listener<String> { response ->
                 Log.d("Response", response.toString())
                 Toast.makeText(this, "OK", Toast.LENGTH_SHORT).show()
+                binding.textView2.text = response
             },
             Response.ErrorListener { error ->
                 Log.d("Error.Response", error.toString())
@@ -59,8 +64,22 @@ class MainActivity : AppCompatActivity() {
                 headers["access-token"] = accessToken
                 return headers
             }
+
+
         }
 
         queue.add(stringRequest)
     }
+
+    fun getDate(x:Int):String {
+        val now = Date()
+        val c: Calendar = Calendar.getInstance()
+        c.setTime(now)
+        c.add(Calendar.HOUR,1)
+        c.add(Calendar.MINUTE, x)
+        val later: Date = c.getTime()
+        val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+        return sdf.format(later)
+    }
+
 }
